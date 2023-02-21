1f56e1f417c62acf7bf80d3e2c682ca046abc592
vacabun <maguotong66@gmail.com>
2023-02-21 22:18:34 +0800

    update test script.

--- setup/locate_test.sh
+++ setup/locate_test.sh
@@ -4,7 +4,8 @@ CURRENT_DIR=$(cd $(dirname $0); pwd)
 WS_DIR=$(cd $(dirname $0);cd ..; pwd)
 
 
-. $WS_DIR/install/setup.sh
+source $WS_DIR/install/setup.sh
+source /opt/ros/humble/setup.sh
 
-ros2 run uwb_location uwb_locate
+ros2 run uwb_locate uwb_location
 
--- setup/sim_test.sh
+++ setup/sim_test.sh
@@ -4,7 +4,8 @@ CURRENT_DIR=$(cd $(dirname $0); pwd)
 WS_DIR=$(cd $(dirname $0);cd ..; pwd)
 
 
-. $WS_DIR/install/setup.sh
+source $WS_DIR/install/setup.sh
+source /opt/ros/humble/setup.sh
 
-ros2 run uwb_sim uwb_simulation
+ros2 run uwb_sim uwb_simulation --ros-args -p label_name:=x500_2
 
